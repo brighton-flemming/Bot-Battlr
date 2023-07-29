be96d827e13b67c4d0c761b2b47e7844aca95b3d
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-29 17:11:45 +0300

    Mission Passed

--- my-app/src/components/RobotDetails.js
+++ my-app/src/components/RobotDetails.js
@@ -1,25 +1,30 @@
 import React from "react";
 
-const RobotDetails = ({robot}) => {
+const RobotDetails = ({ robot }) => {
   if (!robot) {
-    return <div>No Robots Selected</div>
+    return <div>No Robots Selected</div>;
   }
 
-  return(
-   <div>
-    <h2>{robot.name}</h2>
-    <p>Avatar: <img src={robot.avatar_url} alt={robot.name} style={{ maxWidth: "300px" }}/></p>
-    <p>Health:{robot.health}</p>
-    <p>Damage:{robot.damage}</p>
-    <p>Armor:{robot.armor}</p>
-    <p>Bot Class:{robot.bot_class}</p>
-    <p>Catchphrase:{robot.catchphrase}</p>
-    <p>Created At:{robot.created_at}</p>
-    <p>Updated At:{robot.updated_at}</p>
-   </div>
+  return (
+    <div>
+      <h2>{robot.name}</h2>
+      <p>
+        Avatar:{" "}
+        <img
+          src={robot.avatar_url}
+          alt={robot.name}
+          style={{ maxWidth: "300px" }}
+        />
+      </p>
+      <p>Health:{robot.health}</p>
+      <p>Damage:{robot.damage}</p>
+      <p>Armor:{robot.armor}</p>
+      <p>Bot Class:{robot.bot_class}</p>
+      <p>Catchphrase:{robot.catchphrase}</p>
+      <p>Created At:{robot.created_at}</p>
+      <p>Updated At:{robot.updated_at}</p>
+    </div>
+  );
+};
 
-  )
-
-}
-
-export default RobotDetails
+export default RobotDetails;

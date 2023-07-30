8b1de16140424f1b712a45819f6cdbfaa81f274b
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-30 13:56:50 +0300

    Mission Passed

--- my-app/src/components/RobotArmy.js
+++ my-app/src/components/RobotArmy.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const RobotArmy = ({ enlistedRobots }) => {
+const RobotArmy = ({ enlistedRobots, onRelease, onDischarge }) => {
   return (
     <div>
       <h2> Your Robot Army</h2>
@@ -8,11 +8,13 @@ const RobotArmy = ({ enlistedRobots }) => {
         {enlistedRobots.map((robot) => (
           <li key={robot.id}>
             {robot.name} ~ {robot.bot_class}
+            <button onClick={() => onRelease(robot)}>Release</button>
+            <button onClick={() => onDischarge(robot)}>Discharge</button>
           </li>
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
 export default RobotArmy;

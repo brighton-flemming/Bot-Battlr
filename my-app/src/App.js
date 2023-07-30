b1f4bd43e7d4879ebe793a074d816aaef77ae39f
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-30 13:20:50 +0300

    Mission Passed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,31 @@ import RobotArmy from './components/RobotArmy';
 import RobotList from './components/RobotList';
 import './App.css';
 
+const App = () => {
+    const [selectedRobot, setSelectedRobot] = useState(null)
+    const [enlistedRobots, setEnlistedRobots] = useState([]);
 
+    const handleSelectRobot = (robot) => {
+        setSelectedRobot(robot)
+    }
+
+    const handleEnlistRobot = (robot) => {
+        if(selectedRobot && !enlistedRobots.includes(selectedRobot)) {
+            setEnlistedRobots([...enlistedRobots, selectedRobot])
+        }
+    }
+
+   return(
+   <div>
+     <h1>Bot Battlr</h1>
+     <RobotList robots={robots} onSelectRobot={handleSelectRobot} />
+     <RobotDetails robot={selectedRobot} onEnlistRobot={handleEnlistRobot} />
+    <RobotArmy enlistedRobots={enlistedRobots} />
+
+   </div>
+
+   )
+
+}
 
 export default App;

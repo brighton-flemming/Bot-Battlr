f785a0c326319cb8072d66d61ea6b3c4003f64b7
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-30 16:58:33 +0300

    Mission Passed

--- my-app/src/components/RobotDetails.js
+++ my-app/src/components/RobotDetails.js
@@ -1,18 +1,75 @@
-import React from "react";
-import RobotCard from "./RobotCard";
 
-const RobotDetails = ({ robot }) => {
+import React from "react";
+import RobotCard from "./RobotCard"; 
+
+const botTypeClasses = {
+  Assault: "icon military",
+  Defender: "icon shield",
+  Support: "icon plus circle",
+  Medic: "icon ambulance",
+  Witch: "icon magic",
+  Captain: "icon star",
+};
+
+const RobotDetails = ({ robot, onEnlistRobot }) => {
   if (!robot) {
     return <div>No Robots Selected</div>;
   }
 
   return (
-    <div>
-      <h2>{robot.name}</h2>
-      <RobotCard bot={robot} />
-  
+    <div className="ui segment">
+      <div className="ui two column centered grid">
+        <div className="row">
+          <div className="four wide column">
+            <RobotCard bot={robot} /> 
+          </div>
+          <div className="four wide column">
+            <h2>Name: {robot.name}</h2>
+            <p>
+              <strong>Catchphrase: </strong>
+              {robot.catchphrase}
+            </p>
+            <strong>
+              Class: {robot.bot_class}
+              <i className={botTypeClasses[robot.bot_class]} />
+            </strong>
+            <br />
+            <div className="ui segment">
+              <div className="ui three column centered grid">
+                <div className="row">
+                  <div className="column">
+                    <i className="icon large circular red heartbeat" />
+                    <strong>{robot.health}</strong>
+                  </div>
+                  <div className="column">
+                    <i className="icon large circular yellow lightning" />
+                    <strong>{robot.damage}</strong>
+                  </div>
+                  <div className="column">
+                    <i className="icon large circular blue shield" />
+                    <strong>{robot.armor}</strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button
+              className="ui button fluid"
+              onClick={() => console.log("connect this to a function that shows all bots")}
+            >
+              Go Back
+            </button>
+            <button
+              className="ui button fluid"
+              onClick={onEnlistRobot} 
+            >
+              Enlist
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default RobotDetails;
+

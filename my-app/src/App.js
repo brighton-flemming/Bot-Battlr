8b23c73cfe785b26e3d0bd4b4e8c026bdde5e8dd
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-30 13:22:40 +0300

    Mission Passed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,31 @@
-import React, { useState} from 'react';
-import RobotDetails from './components/RobotDetails';
-import RobotArmy from './components/RobotArmy';
-import RobotList from './components/RobotList';
-import './App.css';
+import React, { useState } from "react";
+import RobotDetails from "./components/RobotDetails";
+import RobotArmy from "./components/RobotArmy";
+import RobotList from "./components/RobotList";
+import "./App.css";
 
 const App = () => {
-    const [selectedRobot, setSelectedRobot] = useState(null)
-    const [enlistedRobots, setEnlistedRobots] = useState([]);
+  const [selectedRobot, setSelectedRobot] = useState(null);
+  const [enlistedRobots, setEnlistedRobots] = useState([]);
 
-    const handleSelectRobot = (robot) => {
-        setSelectedRobot(robot)
+  const handleSelectRobot = (robot) => {
+    setSelectedRobot(robot);
+  };
+
+  const handleEnlistRobot = (robot) => {
+    if (selectedRobot && !enlistedRobots.includes(selectedRobot)) {
+      setEnlistedRobots([...enlistedRobots, selectedRobot]);
     }
+  };
 
-    const handleEnlistRobot = (robot) => {
-        if(selectedRobot && !enlistedRobots.includes(selectedRobot)) {
-            setEnlistedRobots([...enlistedRobots, selectedRobot])
-        }
-    }
-
-   return(
-   <div>
-     <h1>Bot Battlr</h1>
-     <RobotList robots={robots} onSelectRobot={handleSelectRobot} />
-     <RobotDetails robot={selectedRobot} onEnlistRobot={handleEnlistRobot} />
-    <RobotArmy enlistedRobots={enlistedRobots} />
-
-   </div>
-
-   )
-
-}
+  return (
+    <div>
+      <h1>Bot Battlr</h1>
+      <RobotList robots={robots} onSelectRobot={handleSelectRobot} />
+      <RobotDetails robot={selectedRobot} onEnlistRobot={handleEnlistRobot} />
+      <RobotArmy enlistedRobots={enlistedRobots} />
+    </div>
+  );
+};
 
 export default App;
